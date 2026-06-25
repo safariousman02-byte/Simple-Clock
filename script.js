d4851126eeb93fc7fdb50805cfdfea05849e96a9
safariousman02-byte <safariousman02@gmail.com>
2026-06-25 20:56:02 +0100

    completing the js file

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+const btn = document.getElementById('btn');
+
+function getTime() {
+
+    fetch('https://worldtimeapi.org/api/timezone/Africa/Lagos')
+        .then(res => res.json())
+        .then(data => {
+
+            const d = new Date(data.datetime);
+
+            time.textContent = d.toLocaleTimeString();
+            date.textContent = d.toLocaleDateString('en-Us', {
+                weekly: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            });
+        })
+        .catch(() => {
+            time.textContent = 'Unable to reach the current time...';
+            date.textContent = 'Unable to reach the current date...';
+        });
+}
+
+
+btn.onclick = getTime;
+getTime();
